dee519141f76e00afe55b551c358744045c05929
Harrison <harrisonmcluckie@gmail.com>
2026-08-20 14:02:33 +1000

    added working image functionality

--- main.java
+++ main.java
@@ -8,13 +8,27 @@ class GFG {
         JButton button = new JButton("GFG Click");
 
         button.setBounds(150,200,220,50);
-
-        frame.add(button);
-
+        JLabel helmet = new JLabel(loadImage("./helm1.png"));
+        helmet.setBounds(150,200,220,100); 
+        //frame.add(button);
+        frame.add(helmet);
         frame.setSize(500,600);
         frame.setLayout(null);
         frame.setVisible(true);
     }
 
+    public static ImageIcon loadImage(String URL)
+    {
+        if(URL != null)
+        {
+            System.out.println("Loaded image" + URL);
+            return new ImageIcon(URL);
+        }
+        else
+        {
+            System.out.println("Failed to load image!");
+            return new ImageIcon("./fallback.jpg");
+        }
 
+    }
 }
